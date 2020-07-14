4dac249d1749696243ebf575396e37734a525140
Alexander González Covic <ag.covic@gmail.com>
2020-07-14 15:13:22 +0100

    Drawing Added

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ const defaultGrid = 3;
 
 divsketch = document.querySelector(".sketch");
 
+let color = [0, 0, 0];
+
+let clicked = false;
+
+window.addEventListener("mousedown", (e) => {
+
+    clicked = true;
+    window.onmousemove = (e) => {
+        if (e.target.className == "grid-item") {
+            e.target.style.background = createRGB(color[0], color[1], color[2]);
+        }
+    }    
+})
+
+window.addEventListener("mouseup", () =>{
+    window.onmousemove = null;
+});
+
+window.addEventListener("drag", () =>{
+    window.onmousemove = null;
+});
+
+function createRGB(r, g, b){
+    return "rgb("+r+","+g+","+b+")";
+}
+
 function createDivs(numberGrid){
     
     let nAuto = "";
